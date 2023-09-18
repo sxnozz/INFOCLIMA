13d821941f1121bffa7e8f44c749276cce39b29e
jplamaral <113566329+jplamaral@users.noreply.github.com>
2023-09-18 20:19:36 -0300

    Create Login.java

---
+++ Login.java
@@ -0,0 +1,21 @@
+public class Login{
+
+    private char email;
+    private char Senha;
+    public char getEmail() {
+        return email;
+    }
+    public void setEmail(char email) {
+        this.email = email;
+    }
+    public char getSenha() {
+        return Senha;
+    }
+    public void setSenha(char senha) {
+        Senha = senha;
+    }
+
+
+
+  
+}
